ced696b8dcbba1a8c19f9508164296cce7781d9f
tenkinogawa <welian@student.agh.edu.pl>
2022-03-06 22:14:00 +0100

    lab 1 - ex1

---
+++ Lab1/lab1_Python_reviev_ex1.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('Data1.csv', index_col=0)
+
+# time series chart
+df.plot()
+
+# histogram chart - all parameters
+df.hist(figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8)
+
+# KDE
+df.plot.kde()
+
+# # repeated for 2018 year
+# df.iloc[365:730] or df.loc['2018-01-01':'2018-12-31']
+df.loc['2018-01-01':'2018-12-31'].plot(y = ['theta_1', 'theta_2', 'theta_3', 'theta_4'])
+
+# histogram chart - 4 paramet  ers
+df.loc['2018-01-01':'2018-12-31'].hist(column = ['theta_1', 'theta_2', 'theta_3', 'theta_4'], figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8)
+
+# KDE
+df.loc['2018-01-01':'2018-12-31'].kde(y = ['theta_1', 'theta_2', 'theta_3', 'theta_4'])
+plt.show()
