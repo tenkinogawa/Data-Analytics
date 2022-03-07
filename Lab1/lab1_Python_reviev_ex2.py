c3d6448fa3ac1ce1b2eb96e362cec986145a9639
tenkinogawa <welian@student.agh.edu.pl>
2022-03-07 22:37:23 +0100

    lab 1 ex 2

--- Lab1/lab1_Python_reviev_ex2.py
+++ Lab1/lab1_Python_reviev_ex2.py
@@ -1,4 +1,30 @@
-import pandas as pd
+import cmdstanpy
+import numpy
 import matplotlib.pyplot as plt
-import numpy as np
+from cmdstanpy import CmdStanModel
+import os
 
+stan_data = {
+    "N":12,
+    "y":[0,1,0,1,0,0,0,1,0,1,0,1]
+}
+
+model = CmdStanModel(stan_file='bern_1.stan')
+sample = model.sample(stan_data)
+theta = sample.stan_variable('theta')
+summary = sample.summary()
+
+q5=numpy.quantile(theta,0.05)
+q50=numpy.quantile(theta,0.5)
+q95=numpy.quantile(theta,0.95)
+qMean=theta.mean()
+
+plt.figure()
+plt.hist(theta, bins=50)
+plt.axvline(q5, color='r')
+plt.axvline(q50, color='g')
+plt.axvline(q95, color='b')
+plt.axvline(qMean, color='k')
+plt.grid(True)
+plt.legend(['5%','Median','95%','Mean'])
+plt.show()
